e0da9fbf2c4ca78a0bd2cf077604285389529241
unknown <santiago.amaya-z@labinfo.is.escuelaing.edu.co>
2025-08-22 09:25:17 -0500

    Implementación Punto 2

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,21 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		List<PrimeFinderThread> threads = new ArrayList<>();
+		Object lockObject = new Object();
+
+		threads.add( new PrimeFinderThread(0,10000000,1));
+		threads.add( new PrimeFinderThread(10000001,20000000,2));
+		threads.add( new PrimeFinderThread(20000001,30000000,3));
+		for (PrimeFinderThread	thread : threads) {
+			thread.start();
+		}
+
 	}
 	
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -6,25 +6,25 @@ import java.util.List;
 public class PrimeFinderThread extends Thread{
 
 	
-	int a,b;
+	private int a,b,t;
 	
 	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
+	private boolean execution;
+	private Object lockObject;
+	public PrimeFinderThread(int a, int b, int t) {
 		this.a = a;
 		this.b = b;
+		this.t = t;
 	}
+
 
 	public void run(){
 		for (int i=a;i<=b;i++){						
 			if (isPrime(i)){
 				primes.add(i);
-				System.out.println(i);
+				System.out.println("Thread " + t + " found prime:" + i);
 			}
 		}
-		
-		
 	}
 	
 	boolean isPrime(int n) {
@@ -39,8 +39,5 @@ public class PrimeFinderThread extends Thread{
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
 	
 }
